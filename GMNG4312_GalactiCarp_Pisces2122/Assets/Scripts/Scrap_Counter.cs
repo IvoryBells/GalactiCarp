ed0dbbdd4bf1a9c004add5875f74b33f318c0cf5
MarianoRmz <Marianoramirez824@yahoo.com>
2022-03-01 21:33:28 -0600

    scrap counter update
    now saw scrap collected x(10*y)

--- GMNG4312_GalactiCarp_Pisces2122/Assets/Scripts/Scrap_Counter.cs
+++ GMNG4312_GalactiCarp_Pisces2122/Assets/Scripts/Scrap_Counter.cs
@@ -6,10 +6,26 @@ public class Scrap_Counter : MonoBehaviour
 {
 
     static public int scrapCount = 0;
+    int scrap_goal;
 
     void OnGUI()
     {
         GUI.skin.box.fontSize = 35;
-        GUI.Box(new Rect(10, 10, 400, 100), "Scrap collected: " + scrapCount);
+
+        /*if (scrapCount < 10)
+        {
+            scrap_goal = 10;
+        }
+        */
+        for(int i = 0;i < 5; i++)
+        {
+            if (scrapCount<(i+1)*10)
+            {
+                scrap_goal = (i+1) * 10;
+                break;
+            }
+        }
+
+        GUI.Box(new Rect(10, 10, 400, 100), "Scrap collected: " + scrapCount + "/" + scrap_goal);
     }
 }
