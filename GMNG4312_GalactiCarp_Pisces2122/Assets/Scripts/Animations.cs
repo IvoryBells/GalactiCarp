6a6c8f2229d7ac3c245c6b6fddd8c9ae7910db9d
Z-Dub89 <94818823+Z-Dub89@users.noreply.github.com>
2022-05-06 11:31:44 -0500

    Properly working animation script
    -The animation script should work fine as with Carpers 4th and Final upgrade stage.
    - Other models may need their Animatoion rig.

--- GMNG4312_GalactiCarp_Pisces2122/Assets/Scripts/Animations.cs
+++ GMNG4312_GalactiCarp_Pisces2122/Assets/Scripts/Animations.cs
@@ -8,6 +8,7 @@ public class Animations : MonoBehaviour
 
     int isWalkingHash;
     int isJumpingHash;
+    int isShootingHash;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,16 +22,19 @@ public class Animations : MonoBehaviour
     {
         bool isWalking = animate.GetBool(isWalkingHash);
         bool isJumping = animate.GetBool(isJumpingHash);
-        bool forwardPress = Input.GetKey("w");
+        bool forwardPress1 = Input.GetKey("w");
+        bool forwardPress2 = Input.GetKey("a");
+        bool forwardPress3 = Input.GetKey("s");
+        bool forwardPress4 = Input.GetKey("d");
         bool jumpPress = Input.GetKey(KeyCode.Space);
 
-        if(!isWalking && forwardPress)
+        if(!isWalking && (forwardPress1 || forwardPress2  || forwardPress3 || forwardPress4))
         {
             animate.SetBool(isWalkingHash, true);
 
         }
 
-        if (isWalking && !forwardPress)
+        if (isWalking && !(forwardPress1 || forwardPress2 || forwardPress3 || forwardPress4))
         {
             animate.SetBool(isWalkingHash, false);
 
